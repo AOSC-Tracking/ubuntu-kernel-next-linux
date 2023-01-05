9a397ab1b266845e38a44d29b6f096905bbda39c
Mika Kuoppala <mika.kuoppala@linux.intel.com>
2023-01-05 11:06:00 +0200

    drm/xe/eudebug: Introduce eudebug support
    BugLink: https://bugs.launchpad.net/bugs/2080263
    
    With eudebug event interface, user space debugger process (like gdb)
    is able to keep track of resources created by another process
    (debuggee using drm/xe) and act upon these resources.
    
    For example, debugger can find a client vm which contains isa/elf
    for a particular shader/eu-kernel and then inspect and modify it
    (for example installing a breakpoint).
    
    Debugger first opens a connection to xe with a drm ioctl specifying
    target pid to connect. This returns an anon fd handle that can then be
    used to listen for events with dedicated ioctl.
    
    This patch introduces eudebug connection and event queuing, adding
    client create/destroy and vm create/destroy events as a baseline.
    More events for full debugger operation are needed and
    those will be introduced in follow up patches.
    
    The resource tracking parts are inspired by the work of
    Maciej Patelczyk on resource handling for i915. Chris Wilson
    suggested improvement of two ways mapping which makes it easy to
    use resource map as a definitive bookkeep of what resources
    are played to debugger in the discovery phase (on follow up patch).
    
    v2: - Kconfig support (Matthew)
        - ptraced access control (Lucas)
        - pass expected event length to user (Zbigniew)
        - only track long running VMs
        - checkpatch (Tilak)
        - include order (Andrzej)
        - 32bit fixes (Andrzej)
        - cleaner get_task_struct
        - remove xa_array and use clients.list for tracking (Mika)
    
    v3: - adapt to removal of clients.lock (Mika)
        - create_event cleanup (Christoph)
    
    Cc: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    Cc: Dominik Grzegorzek <dominik.grzegorzek@intel.com>
    Cc: Andi Shyti <andi.shyti@linux.intel.com>
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: Zbigniew Kempczyński <zbigniew.kempczynski@intel.com>
    Cc: Andrzej Hajda <andrzej.hajda@intel.com>
    
    Signed-off-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Signed-off-by: Maciej Patelczyk <maciej.patelczyk@intel.com>
    Signed-off-by: Dominik Grzegorzek <dominik.grzegorzek@intel.com>
    Signed-off-by: Jonathan Cavitt <jonathan.cavitt@intel.com>
    Signed-off-by: Christoph Manszewski <christoph.manszewski@intel.com>
    Signed-off-by: Kolanupaka Naveena <kolanupaka.naveena@intel.com>
    (backported from commit a807a89f021ecc320e4fe8e1381992ae8e870fee eudebug-dev)
    Signed-off-by: S A Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Bommu Krishnaiah <krishnaiah.bommu@intel.com>
    Signed-off-by: Thibault Ferrante <thibault.ferrante@canonical.com>

---
+++ drivers/gpu/drm/xe/xe_eudebug.h
@@ -0,0 +1,46 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright © 2023 Intel Corporation
+ */
+
+#ifndef _XE_EUDEBUG_H_
+
+struct drm_device;
+struct drm_file;
+struct xe_device;
+struct xe_file;
+struct xe_vm;
+
+#if IS_ENABLED(CONFIG_DRM_XE_EUDEBUG)
+
+int xe_eudebug_connect_ioctl(struct drm_device *dev,
+			     void *data,
+			     struct drm_file *file);
+
+void xe_eudebug_init(struct xe_device *xe);
+void xe_eudebug_fini(struct xe_device *xe);
+
+void xe_eudebug_file_open(struct xe_file *xef);
+void xe_eudebug_file_close(struct xe_file *xef);
+
+void xe_eudebug_vm_create(struct xe_file *xef, struct xe_vm *vm);
+void xe_eudebug_vm_destroy(struct xe_file *xef, struct xe_vm *vm);
+
+#else
+
+static inline int xe_eudebug_connect_ioctl(struct drm_device *dev,
+					   void *data,
+					   struct drm_file *file) { return 0; }
+
+static inline void xe_eudebug_init(struct xe_device *xe) { }
+static inline void xe_eudebug_fini(struct xe_device *xe) { }
+
+static inline void xe_eudebug_file_open(struct xe_file *xef) { }
+static inline void xe_eudebug_file_close(struct xe_file *xef) { }
+
+static inline void xe_eudebug_vm_create(struct xe_file *xef, struct xe_vm *vm) { }
+static inline void xe_eudebug_vm_destroy(struct xe_file *xef, struct xe_vm *vm) { }
+
+#endif /* CONFIG_DRM_XE_EUDEBUG */
+
+#endif
