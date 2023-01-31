418c1674b677f0b741d5016d8fbeb9b5a75b1300
Mika Kuoppala <mika.kuoppala@linux.intel.com>
2023-01-31 11:40:42 +0200

    drm/xe/eudebug: Introduce discovery for resources
    BugLink: https://bugs.launchpad.net/bugs/2080263
    
    Debugger connection can happen way after the client has
    created and destroyed arbitrary number of resources.
    
    We need to playback all currently existing resources for the
    debugger. The client is held until this so called discovery
    process, executed by workqueue, is complete.
    
    This patch is based on discovery work by Maciej Patelczyk
    for i915 driver.
    
    v2: - use rw_semaphore to block drm_ioctls during discovery (Matthew)
        - only lock according to ioctl at play (Dominik)
    
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: Dominik Grzegorzek <dominik.grzegorzek@intel.com>
    Co-developed-by: Maciej Patelczyk <maciej.patelczyk@intel.com>
    Signed-off-by: Maciej Patelczyk <maciej.patelczyk@intel.com>
    Signed-off-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    Acked-by: Matthew Brost <matthew.brost@intel.com> #locking
    Signed-off-by: Kolanupaka Naveena <kolanupaka.naveena@intel.com>
    (backported from commit 9dde4e4ba3dd91c82aa297112001659956089c30 eudebug-dev)
    Signed-off-by: S A Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Bommu Krishnaiah <krishnaiah.bommu@intel.com>
    Signed-off-by: Thibault Ferrante <thibault.ferrante@canonical.com>

--- drivers/gpu/drm/xe/xe_device.c
+++ drivers/gpu/drm/xe/xe_device.c
@@ -209,8 +209,11 @@ static long xe_drm_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 		return -ECANCELED;
 
 	ret = xe_pm_runtime_get_ioctl(xe);
-	if (ret >= 0)
+	if (ret >= 0) {
+		xe_eudebug_discovery_lock(xe, cmd);
 		ret = drm_ioctl(file, cmd, arg);
+		xe_eudebug_discovery_unlock(xe, cmd);
+	}
 	xe_pm_runtime_put(xe);
 
 	return ret;
@@ -227,8 +230,11 @@ static long xe_drm_compat_ioctl(struct file *file, unsigned int cmd, unsigned lo
 		return -ECANCELED;
 
 	ret = xe_pm_runtime_get_ioctl(xe);
-	if (ret >= 0)
+	if (ret >= 0) {
+		xe_eudebug_discovery_lock(xe, cmd);
 		ret = drm_compat_ioctl(file, cmd, arg);
+		xe_eudebug_discovery_unlock(xe, cmd);
+	}
 	xe_pm_runtime_put(xe);
 
 	return ret;
