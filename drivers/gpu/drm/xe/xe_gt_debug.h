2b12e419d1cda74edea6d9c07896e6449637d6ce
Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
2024-11-11 12:34:59 +0200

    drm/xe/eudebug: Add read/count/compare helper for eu attention
    BugLink: https://bugs.launchpad.net/bugs/2091444
    
    Add xe_eu_attentions structure to capture and store eu attention bits.
    Add a function to count the number of eu threads that have turned on from
    eu attentions, and add a function to count the number of eu threads that
    have changed on a state between eu attentions.
    
    Signed-off-by: Gwan-gyeong Mun <gwan-gyeong.mun@intel.com>
    Signed-off-by: Mika Kuoppala <mika.kuoppala@linux.intel.com>
    (cherry picked from commit 9f5abd3965f54eb848eb2a477b307d6fe09336d1 eudebug-dev)
    Signed-off-by: S A Muqthyar Ahmed <syed.abdul.muqthyar.ahmed@intel.com>
    Signed-off-by: Thibault Ferrante <thibault.ferrante@canonical.com>

--- drivers/gpu/drm/xe/xe_gt_debug.h
+++ drivers/gpu/drm/xe/xe_gt_debug.h
@@ -12,6 +12,15 @@
 
 #define XE_GT_ATTENTION_TIMEOUT_MS 100
 
+struct xe_eu_attentions {
+#define XE_MAX_EUS 1024
+#define XE_MAX_THREADS 10
+
+	u8 att[DIV_ROUND_UP(XE_MAX_EUS * XE_MAX_THREADS, BITS_PER_BYTE)];
+	unsigned int size;
+	ktime_t ts;
+};
+
 int xe_gt_eu_threads_needing_attention(struct xe_gt *gt);
 int xe_gt_foreach_dss_group_instance(struct xe_gt *gt,
 				     int (*fn)(struct xe_gt *gt,
@@ -24,4 +33,10 @@ int xe_gt_eu_attention_bitmap_size(struct xe_gt *gt);
 int xe_gt_eu_attention_bitmap(struct xe_gt *gt, u8 *bits,
 			      unsigned int bitmap_size);
 
+void xe_gt_eu_attentions_read(struct xe_gt *gt,
+			      struct xe_eu_attentions *a,
+			      const unsigned int settle_time_ms);
+
+unsigned int xe_eu_attentions_xor_count(const struct xe_eu_attentions *a,
+					const struct xe_eu_attentions *b);
 #endif
