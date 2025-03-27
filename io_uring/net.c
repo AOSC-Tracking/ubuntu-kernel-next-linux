1ec82a213d0a7516297b23b84c66a3073025f3be
Pavel Begunkov <asml.silence@gmail.com>
2025-03-27 09:57:27 +0000

    io_uring/net: fix io_req_post_cqe abuse by send bundle
    BugLink: https://bugs.launchpad.net/bugs/2111782
    
    commit 6889ae1b4df1579bcdffef023e2ea9a982565dff upstream.
    
    [  114.987980][ T5313] WARNING: CPU: 6 PID: 5313 at io_uring/io_uring.c:872 io_req_post_cqe+0x12e/0x4f0
    [  114.991597][ T5313] RIP: 0010:io_req_post_cqe+0x12e/0x4f0
    [  115.001880][ T5313] Call Trace:
    [  115.002222][ T5313]  <TASK>
    [  115.007813][ T5313]  io_send+0x4fe/0x10f0
    [  115.009317][ T5313]  io_issue_sqe+0x1a6/0x1740
    [  115.012094][ T5313]  io_wq_submit_work+0x38b/0xed0
    [  115.013223][ T5313]  io_worker_handle_work+0x62a/0x1600
    [  115.013876][ T5313]  io_wq_worker+0x34f/0xdf0
    
    As the comment states, io_req_post_cqe() should only be used by
    multishot requests, i.e. REQ_F_APOLL_MULTISHOT, which bundled sends are
    not. Add a flag signifying whether a request wants to post multiple
    CQEs. Eventually REQ_F_APOLL_MULTISHOT should imply the new flag, but
    that's left out for simplicity.
    
    Cc: stable@vger.kernel.org
    Fixes: a05d1f625c7aa ("io_uring/net: support bundles for send")
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Link: https://lore.kernel.org/r/8b611dbb54d1cd47a88681f5d38c84d0c02bc563.1743067183.git.asml.silence@gmail.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: Noah Wager <noah.wager@canonical.com>
    Signed-off-by: Mehmet Basaran <mehmet.basaran@canonical.com>

--- io_uring/net.c
+++ io_uring/net.c
@@ -435,6 +435,7 @@ int io_sendmsg_prep(struct io_kiocb *req, const struct io_uring_sqe *sqe)
 		sr->msg_flags |= MSG_WAITALL;
 		sr->buf_group = req->buf_index;
 		req->buf_list = NULL;
+		req->flags |= REQ_F_MULTISHOT;
 	}
 	if (req->flags & REQ_F_BUFFER_SELECT && sr->len)
 		return -EINVAL;
